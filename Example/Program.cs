69498fda7a8dbd6000ec4bb6d3de742268dc4d05
Beisenbek Baisakov <beisenbek@gmail.com>
2016-01-23 17:32:48 +0600

    Example with Console Operations

---
+++ Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.SetCursorPosition(10, 10);
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                //Console.Clear();
+
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("up");
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("down");
+                        break;
+                }
+            }
+
+            Console.Write("*");
+        }
+    }
+}
